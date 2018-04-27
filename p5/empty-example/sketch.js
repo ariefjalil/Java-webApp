adb7ea9210d3fb702467cbd7d1e06753cd864b1d
ariefjalil <up.prozac3@gmail.com>
2018-04-27 20:59:30 +0800

    stream of code
    :q

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,5 +1,7 @@
-var symbol;
-var symbolSize = 40;
+var symbolSize = 30;
+//var alphabet = "tradeshift";
+var stream;
+
 
 function setup(){
 
@@ -8,21 +10,15 @@ function setup(){
 			window.innerHeight
 		);
 	background(0);
-	symbol = new Symbol(
-		width / 2,
-		//height / 2,
-		0,
-		random(5, 10)
-
-		);
-symbol.setToRandomSymbol();
-textSize(symbolSize);
+	stream = new Stream();
+	stream.generateSymbols();
+	textSize(symbolSize);
 
 }
 
 function draw(){
 		background(0);
-		symbol.render();
+		stream.render();
 		//console.log("java");
 		
 
@@ -33,20 +29,17 @@ function Symbol(x,y, speed){
 	this.y = y;
 	this.value;
 	this.speed = speed;
+	this.switchInterval = round(random(2,20)); 
 
 	this.setToRandomSymbol = function() {
+		if(frameCount % this.switchInterval ==0){
 		this.value = String.fromCharCode(
 			0x30A0 + round(random(0,96))
-
+			//0x0041 + round(random(0,25))
 			);
-
+			}
 		}
-		this.render = function(){
-			fill(0,255,70);
-			text(this.value, this.x, this.y);
-			this.rain();
 
-		}
 			this.rain = function(){
 				if (this.y >= height){
 					this.y = 0;
@@ -59,9 +52,40 @@ function Symbol(x,y, speed){
 	}
 
 
-
-
-function stream(){
+function Stream(){
 //to view stream of chars
+	this.symbols = [];
+	this.totalSymbols = round(random(5,30));
+	this.speed = random(1,7);
+
+	this.generateSymbols = function() {
+		var y = 0;
+		var x = width / 2;
+
+		for (var i =0; i<= this.totalSymbols; i++) {
+			symbol = new Symbol(x, y, this.speed);
+			symbol.setToRandomSymbol();
+			this.symbols.push(symbol);
+			y -= symbolSize;
+
+
+		}
+
+	}
+
+	this.render = function() {
+
+		this.symbols.forEach(function(symbol)  {
+			fill (0,255,70);
+			text(symbol.value, symbol.x, symbol.y);
+			symbol.rain();
+			symbol.setToRandomSymbol();
+
+
+		});
+	}
+
+
+
 
 }
