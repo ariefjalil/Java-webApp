b94d7a2deb39d74ff60a4474f8612688e303575a
ariefjalil <up.prozac3@gmail.com>
2018-09-07 17:08:26 +0800

    added update function

--- p5/Particle/sketch.js
+++ p5/Particle/sketch.js
@@ -11,6 +11,7 @@ function draw() {
     particles.push(p);
   }
   for (let i = particles.length - 1; i >= 0; i--) {
+    particles[i].update();
     particles[i].show();
     if (particles[i].finished()) {
       // remove this particle
@@ -24,7 +25,7 @@ class Particle {
   constructor() {
     this.x = 300;
     this.y = 380;
-    this.vx = random(-1, 1);
+    this.vx = random(-1, 2);
     this.vy = random(-5, -1);
     this.alpha = 255;
   }
@@ -33,6 +34,12 @@ class Particle {
     return this.alpha < 0;
   }
 
+  update() {
+    this.x += this.vx;
+    this.y += this.vy;
+    this.alpha -= 5;
+
+  }
 
 
   show() {
